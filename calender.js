86c57b14f0f716b640897a334040a482e17e9561
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-03-11 16:28:34 -0700

    added timezones to calender page

--- calender.js
+++ calender.js
@@ -1,10 +1,18 @@
 function loadCalender() {
-  document.getElementById('main').innerHTML = `<div id="calender-text"><p>Missing your appointment because of time zone differences is considered a missed appointment. We are in the Mountain Standard Time Zone all year around. Check to make certain you know the difference in time before scheduling your appointment.
-  If you are not familiar with time zones, please refer to the chart below.
-  <p>During Daylight Savings Time
-  5:00 PM MST (our time) is the same as . . .
-  5:00 PDT, 6:00 MDT, 7:00 CDT, 8:00 EDT</p>
-  <p>During Standard Time
-  5:00 PM MST (our time) is the same as . . .
-  4:00 PST, 5:00 MST, 6:00 CST, 7:00 EST</p></div>`;
+  document.getElementById('main').innerHTML = `<div id="calender-text"><p><b>Missing your appointment because of time zone differences is considered a missed appointment. We are in the Mountain Standard Time Zone all year around. Check to make certain you know the difference in time before scheduling your appointment. If you are not familiar with time zones, please refer to the chart below.</b>
+
+  <div class="row times">
+    <div class="col s12 m4 l4">
+      <div style="text-align:center;width:400px;padding:1em 0;"><a style="text-decoration:none;" href="http://www.zeitverschiebung.net/en/city/5318313">Mountain Standard Time</a><iframe src="http://www.zeitverschiebung.net/clock-widget-iframe-v2?language=en&timezone=America%2FPhoenix" width="100%" height="150" frameborder="0" seamless></iframe><small style="color:gray;">&copy; <a href="http://www.zeitverschiebung.net/en/" style="color: gray;">Time Difference</a></small>
+      </div>
+    </div>
+    <div class="col s12 m4 l4">
+      <div style="text-align:center;width:400px;padding:1em 0;"><a style="text-decoration:none;" href="http://www.zeitverschiebung.net/en/city/4887398">Central Standard Time</a><iframe src="http://www.zeitverschiebung.net/clock-widget-iframe-v2?language=en&timezone=America%2FChicago" width="100%" height="150" frameborder="0" seamless></iframe> <small style="color:gray;">&copy; <a href="http://www.zeitverschiebung.net/en/" style="color: gray;">Time Difference</a></small>
+      </div>
+    </div>
+    <div class="col s12 m4 l4">
+      <div style="text-align:center;width:400px;padding:1em 0;"><a style="text-decoration:none;" href="http://www.zeitverschiebung.net/en/city/5128581">Eastern Standard Time</a><iframe src="http://www.zeitverschiebung.net/clock-widget-iframe-v2?language=en&timezone=America%2FNew_York" width="100%" height="150" frameborder="0" seamless></iframe> <small style="color:gray;">&copy; <a href="http://www.zeitverschiebung.net/en/" style="color: gray;">Time Difference</a></small>
+      </div>
+    </div>
+  </div>`;
 }
