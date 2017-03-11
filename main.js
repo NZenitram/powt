aaf9dd7ebb3e28631022bd55974edbcc2837c8e1
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-03-11 12:59:59 -0700

    services typed out

--- main.js
+++ main.js
@@ -15,15 +15,31 @@ function loadServices() {
     <div class="col s12 m4 l4">
     <p><b>One on One member coaching for both students and parents.</b>
 
-      Members schedule one on one coaching sessions with their personal coach who has over 35 years of college admission and funding experience with students from all backgrounds. Sessions recorded for your review with detailed directions and video guides</p></div>
+      Members schedule one on one coaching sessions with their personal coach who has over 35 years of college admission and funding experience with students from all backgrounds. Sessions recorded for your review with detailed directions and video guides.</p>
+    <p><b>Financial Aid Form Guidance and Filing Service.</b>
+
+      Assist students and parents with the completion of all financial aid forms including suggested strategies for enhancing the liklihood of receiving both need and especially merit based aid. Over $750,000,000 in NON LOAN awards to our members to date</p>
+
+    <p><b>Negotiation and Financial Aid Strategies</b>
+      Proven techniques for enhancing the awards your student receives using time tested strategies that expand your funding offers.</p>
+  </div>
     <div class="col s12 m4 l4">
     <p><b>PSAT, SAT, ACT in depth preparation guide.</b>
 
-    Provides comprehensive state of the art tutorial services mirroring step by step guide for preparing and excelling in the college preparation and planning process. Timed and geared to tutor student in areas needing improvement to increase scores by 25%</p></div>
+    Provides comprehensive state of the art tutorial services mirroring step by step guide for preparing and excelling in the college preparation and planning process. Timed and geared to tutor student in areas needing improvement to increase scores by 25%.</p>
+
+    <p id="col2-pad"><b>Personality Profile and Career Assessment.</b>
+      Student driven personality and career assessment guides that evaluate student strengths making career recommendations bases on student's areas of interest and personality styles. Highly effective at helping students determine college choices and careers.</p>
+  </div>
     <div class="col s12 m4 l4">
     <p><b>College Selection and Notification to colleges.</b>
 
-    Upon identifying student areas of interest and career direction, POWT identifies multiple colleges, both public and private, that are interested in recruiting student and would be willing to offer funding incentives to assist student in attending.</p></div>
+    Upon identifying student areas of interest and career direction, POWT identifies multiple colleges, both public and private, that are interested in recruiting student and would be willing to offer funding incentives to assist student in attending.</p>
+
+    <p id="col2-pad"><b>Backed by our "Promise of Value"</b>
+
+    Our "Promise of Value" makes a written financial commitment that you will receive value for your participation and membership. You will receive the value promised in writing or ALL dues paid will be returned. You win or you pay nothing.</p>
+  </div>
   </div>`;
 }
 
