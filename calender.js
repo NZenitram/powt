37c6989c6b1105d466cde8fcb480a8795a16beb9
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-03-12 09:03:24 -0600

    login and footer updated

--- calender.js
+++ calender.js
@@ -1,10 +1,9 @@
 function loadCalender() {
   document.getElementById('main').innerHTML = `<div id="calender-text">
   <div id="calender-link">
-    <p>To schedule an appointment, please <a href="http://www.appointmentquest.com/scheduler/2150078737">Click Here</a></p>
+    <p>To schedule an appointment, please <a href="http://www.appointmentquest.com/scheduler/2150078737" target="_blank">Click Here</a></p>
   </div>
   <p><b>Missing your appointment because of time zone differences is considered a missed appointment. We are in the Mountain Standard Time Zone all year around. Check to make certain you know the difference in time before scheduling your appointment. If you are not familiar with time zones, please refer to the clocks below.</b>
-
   <div class="row times">
     <div class="col s12 m4 l4">
       <div style="text-align:center;width:400px;padding:1em 0;"><a style="text-decoration:none;" href="http://www.zeitverschiebung.net/en/city/5318313">Mountain Standard Time</a><iframe src="http://www.zeitverschiebung.net/clock-widget-iframe-v2?language=en&timezone=America%2FPhoenix" width="100%" height="150" frameborder="0" seamless></iframe>
