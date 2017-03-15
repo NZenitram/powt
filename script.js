6111bc270857a6e82d075ab2c0559b6d8ce04c76
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-03-14 21:28:09 -0600

    card added

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
   $('.parallax').parallax();
 });
+
+var divs = $('.card-img');
+if($(window).scrollTop()<10){
+  divs.stop(true,true).fadeIn("fast");
+} else {
+  divs.stop(true,true).fadeOut("fast");
+}
